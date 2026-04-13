bbc083933492152e72e412d3365cd81f0a932560
slwnt31 <slwnt8531@gmail.com>
2026-04-13 21:10:50 +0900

    [Feat] Review 생성 Controller, DTO 작성(#12)

--- src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
@@ -1,4 +1,36 @@
 package com.example.umc10th.domain.review.controller;
 
+import com.example.umc10th.domain.review.dto.ReviewReqDTO;
+import com.example.umc10th.domain.review.dto.ReviewResDTO;
+import com.example.umc10th.domain.review.exception.code.ReviewSuccessCode;
+import com.example.umc10th.domain.review.service.ReviewService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping(
+            value = "/v1/my-page/completed-missions/{userMissionId}/reviews",
+
+            // 리뷰 이미지를 multipartfile로 받겠다는 뜻
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    ) public ApiResponse<ReviewResDTO.CreateReview> createReview (
+            @PathVariable Long userMissionId,
+            @RequestPart("review") ReviewReqDTO.CreateReview dto,
+            @RequestPart(value="images", required=false) List<MultipartFile> images
+    ) {
+        BaseSuccessCode code = ReviewSuccessCode.OK;
+        return ApiResponse.onSuccess(code, reviewService.createReview(userMissionId, dto, images));
+    }
 }
--- src/main/java/com/example/umc10th/domain/review/service/ReviewService.java
+++ src/main/java/com/example/umc10th/domain/review/service/ReviewService.java
@@ -1,4 +1,25 @@
 package com.example.umc10th.domain.review.service;
 
+import com.example.umc10th.domain.review.dto.ReviewReqDTO;
+import com.example.umc10th.domain.review.dto.ReviewResDTO;
+import com.example.umc10th.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    public ReviewResDTO.CreateReview createReview(
+            Long userMissionId,
+            ReviewReqDTO.CreateReview dto,
+            List<MultipartFile> images)
+    {
+        throw new UnsupportedOperationException("아직 구현되지 않은 기능입니다.");
+    }
 }
