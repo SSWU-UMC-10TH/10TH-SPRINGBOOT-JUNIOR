41f6ceb8478a0aece9f5bf5c2eb486429078fc9a
slwnt31 <slwnt8531@gmail.com>
2026-04-13 21:42:31 +0900

    [Feat] 진행중/진행 완료 Mission 조회 Success 코드 추가(#12)

--- src/main/java/com/example/umc10th/domain/mission/exception/code/MissionSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/mission/exception/code/MissionSuccessCode.java
@@ -9,10 +9,16 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MissionSuccessCode implements BaseSuccessCode {
 
-    OK(HttpStatus.OK,
+    HOME_OK(HttpStatus.OK,
             "HOME200_1",
             "성공적으로 사용자/지역별 홈 화면을 조회했습니다."),
+
+    MISSION_OK(HttpStatus.OK,
+            "MISSION200_1",
+            "사용자별 진행중/진행 완료 미션을 성공적으로 조회했습니다.")
     ;
+
+
 
     private final HttpStatus status;
     private final String code;
