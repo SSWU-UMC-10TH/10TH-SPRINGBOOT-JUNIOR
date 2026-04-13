c3483a2d4f58b7006f2f724c3775bda2894b9690
slwnt31 <slwnt8531@gmail.com>
2026-04-13 20:34:14 +0900

    [Feat] Home 조회 Success 코드 추가(#12)

--- src/main/java/com/example/umc10th/domain/mission/exception/code/MissionSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/mission/exception/code/MissionSuccessCode.java
@@ -1,4 +1,20 @@
 package com.example.umc10th.domain.mission.exception.code;
 
-public enum MissionSuccessCode {
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MissionSuccessCode implements BaseSuccessCode {
+
+    OK(HttpStatus.OK,
+            "HOME200_1",
+            "성공적으로 사용자/지역별 홈 화면을 조회했습니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
