70b8997de8f7b4407562feb886bc06896c6b2737
slwnt31 <slwnt8531@gmail.com>
2026-04-13 20:32:45 +0900

    [Fix] User 회원가입 ResDTO @Builder 어노테이션 추가(#12)

--- src/main/java/com/example/umc10th/domain/user/dto/UserResDTO.java
+++ src/main/java/com/example/umc10th/domain/user/dto/UserResDTO.java
@@ -2,10 +2,12 @@ package com.example.umc10th.domain.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
 public class UserResDTO {
 
     // 회원가입
+    @Builder
     public record SignUp(
             String name,
             String nickname,
