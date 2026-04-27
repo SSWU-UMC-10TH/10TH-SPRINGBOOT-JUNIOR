dd42500aa239465e99fe648d7c3b2364909fd736
slwnt31 <slwnt8531@gmail.com>
2026-04-27 21:40:55 +0900

    [Feat] Review Exception 추가(#20)

--- src/main/java/com/example/umc10th/domain/review/exception/ReviewException.java
+++ src/main/java/com/example/umc10th/domain/review/exception/ReviewException.java
@@ -1,7 +1,12 @@
 package com.example.umc10th.domain.review.exception;
 
+import com.example.umc10th.domain.review.exception.code.ReviewErrorCode;
+
 public class ReviewException extends RuntimeException {
-    public ReviewException(String message) {
-        super(message);
-    }
-}
+
+    private final ReviewErrorCode errorCode;
+
+    public ReviewException(ReviewErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }}
